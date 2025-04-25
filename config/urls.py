7aaf8300a2b047f865848ed8aa9ea9b1423f7927
pyaustine <austinjuniour4@gmail.com>
2025-04-25 19:40:22 +0300

    config all routes

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # API endpoints v1
+    path('api/v1/accounts/', include('accounts.urls')),
+    path('api/v1/programs/', include('programs.urls')),
+    path('api/v1/clients/', include('clients.urls')),
+    path('api/v1/enrollments/', include('enrollments.urls')),
 ]
