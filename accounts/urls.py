1395064488aad18c5a1213932f6224fee143e94e
pyaustine <austinjuniour4@gmail.com>
2025-04-25 20:51:44 +0300

    complete logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from .views import RegisterView, LogoutView
 
 urlpatterns = [
     path('auth/', include([
         path('register/', RegisterView.as_view(), name='auth_register'),
         path('login/', TokenObtainPairView.as_view(), name='token_obtain'),
         path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+        path('logout/', LogoutView.as_view(), name='token_logout'),
     ])),
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,27 @@
-from rest_framework import generics, permissions
+# within app imports
 from .serializers import UserSerializer
 from .models import User
 
+# third part imports
+from rest_framework import generics, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
+# register user
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
+
+# logout user
+class LogoutView(APIView):
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
