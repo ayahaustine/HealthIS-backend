a65c18cab4e78557e5140341e003f8013a74efa2
pyaustine <austinjuniour4@gmail.com>
2025-04-26 19:08:35 +0300

    debug production conflicts

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Run Django management commands
-# python manage.py flush --no-input
+# management commands
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
