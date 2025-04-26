ff434ab515c0467d74d823060482ba026e2ad94f
pyaustine <austinjuniour4@gmail.com>
2025-04-26 03:07:04 +0300

    updated

--- programs/serializers.py
+++ programs/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import Program
+from clients.serializers import ClientSerializer
 
 class ProgramSerializer(serializers.ModelSerializer):
     created_by = serializers.StringRelatedField(read_only=True)
     id = serializers.CharField(read_only=True)
+
     
     class Meta:
         model = Program
--- programs/views.py
+++ programs/views.py
@@ -9,7 +9,9 @@ class ProgramViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsCreatorOrReadOnly]
 
     def get_queryset(self):
-        return Program.objects.filter(is_active=True)
+        return Program.objects.filter(
+            is_active=True,
+            created_by=self.request.user).prefetch_related('clients')
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
