6d94e0a56b989bb3e0d402cc96ba6fb783560113
pyaustine <austinjuniour4@gmail.com>
2025-04-27 15:08:18 +0300

    applying ordering and slight improvement

--- public/views.py
+++ public/views.py
@@ -4,14 +4,18 @@ from public.serializers import PublicClientSerializer
 from rest_framework.permissions import AllowAny
 
 class PublicClientListView(generics.ListAPIView):
-    queryset = Client.objects.prefetch_related('enrollments__program').all()
     serializer_class = PublicClientSerializer
     permission_classes = [AllowAny]
     filter_backends = [filters.SearchFilter]
     search_fields = ['first_name', 'last_name', 'uuid', 'county', 'sub_county']
 
+    def get_queryset(self):
+        return Client.objects.prefetch_related('enrollments__program').order_by('-created_at')
+
 class PublicClientRetrieveView(generics.RetrieveAPIView):
-    queryset = Client.objects.prefetch_related('enrollments__program').all()
     serializer_class = PublicClientSerializer
     permission_classes = [AllowAny]
     lookup_field = 'uuid'
+
+    def get_queryset(self):
+        return Client.objects.prefetch_related('enrollments__program')
