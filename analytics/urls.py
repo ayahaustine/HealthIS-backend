2704a8544b85110f3d45624b55b378df6b0c2939
pyaustine <austinjuniour4@gmail.com>
2025-04-27 17:04:36 +0300

    add monthly enrollment SQL queries

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TotalClientsView, ActiveProgramsView, EnrollmentsView
+from .views import *
 
 urlpatterns = [
     path('total_clients/', TotalClientsView.as_view(), name='total_clients'),
     path('active_programs/', ActiveProgramsView.as_view(), name='active_programs'),
     path('enrollments/', EnrollmentsView.as_view(), name='enrollments'),
+    path('monthly_enrollments/', MonthlyEnrollmentsView.as_view(), name='monthly_enrollment'),
+
 ]
